8d7158411bda03441b2a9e19a3bcbc32489ffe92
nafania2022 <nafania1522@gmail.com>
2023-04-29 16:19:42 +0300

    test 4

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -195,6 +195,11 @@ CSRF_COOKIE_HTTPONLY = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/var/www/static/",
+]
+
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
